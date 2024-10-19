1754adc3ab34753746b4fa039ce1acebca0bfa73
Roberto Borges <roberto.borges@outlook.com>
2024-10-19 11:19:21 -0400

    Adding Azure Validation

--- Controllers/OnAttributeCollectionStartController.cs
+++ Controllers/OnAttributeCollectionStartController.cs
@@ -2,6 +2,7 @@ using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 using woodgroveapi.Helpers;
 using woodgroveapi.Models;
 
@@ -25,11 +26,11 @@ public class OnAttributeCollectionStartController : ControllerBase
     public AttributeCollectionStartResponse PostAsync([FromBody] AttributeCollectionRequest requestPayload)
     {
         // For Azure App Service with Easy Auth, validate the azp claim value
-        // if (!AzureAppServiceClaimsHeader.Authorize(this.Request))
-        // {
-        //     Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-        //     return null;
-        // }
+        if (!AzureAppServiceClaimsHeader.Authorize(this.Request))
+        {
+            Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            return null;
+        }
 
         // Track the page view 
         AppInsightsHelper.TrackApi("OnAttributeCollectionStart", this._telemetry, requestPayload.data);
